0f0452018c9cd71efba1a012a9d89c4da78f8f0b
vagrant <vagrant@myvm18>
2021-08-19 06:06:24 +0000

    check precedence of operator

--- Stack_Queue/infix_postfix.c
+++ Stack_Queue/infix_postfix.c
@@ -1,6 +1,5 @@
 /**
- * Program to convert an arithmetic expression from infix notation
-to postfix notation.
+ * Program to convert an arithmetic expression from infix notation to postfix notation.
  * Compilation: gcc -o ip infix_postfix.c
  * Execution: ./ip
  * 
@@ -10,70 +9,137 @@ to postfix notation.
 
 #include <stdio.h>
 
-char a[20];
-int top = -1;
-
-void push(char value)
+struct stack
 {
-    if(top < 19)
+    int top;
+    char *a;
+};
+ 
+// Allocate memory to the stack and initialize it
+struct stack* newStack()
+{
+    struct stack *pt = (struct stack*)malloc(sizeof(struct stack));
+ 
+    pt->top = -1;
+    pt->a = (char*)malloc(sizeof(char) * 10);
+ 
+    return pt;
+}
+
+// Add a new element to the top of the stack
+void push(struct stack* pt,char value)
+{
+    if(pt->top < 9)
     {
-        top += 1;
-        a[top] = value;
+        pt->top += 1;
+        pt->a[pt->top] = value;
     }
     else
         printf("Overflow\n");
 
 }
 
-char pop()
+// Removes and returns the element from the top of the stack
+char pop(struct stack* pt)
 {
-    if(top > -1)
+    if(pt->top > -1)
     {
-        char el = a[top];
-        top -= 1;
+        char el = pt->a[pt->top];
+        pt->top -= 1;
         return el;
     }
     else
         return -1;
 }
 
-char peek()
+// Return but not remove the element at the top of the stack
+char peek(struct stack* pt)
 {
-    if(top > -1)
-        return a[top];
+    if(pt->top > -1)
+        return pt->a[pt->top];
     else
         return -1;
 }
 
-void postfix(char* s)
+// Checks the precedence of the operator
+int priority(char c)
 {
-    int i = 0;
-    while(s[i] != '\0')
+    if(c == '^')
+        return 3;
+    else if(c == '*' || c == '/')
+        return 2;
+    else if(c == '+' || c == '-')
+        return 1;
+    else
+        return 0;
+}
+
+// Converts an arithmetic expression from infix notation to postfix notation
+void postfix(char* inp)
+{
+    // stack to store operators and '('
+    struct stack *operators = newStack();
+    char output[20];
+
+    int i = 0, j = 0;
+    // Traverse input string
+    while(inp[i] != '\0')
     {
-        char el = s[i];
-        if(el == '(' || el == '+' || el == '-' || el == '*' || el == '/')
+        char el = inp[i];
+
+        // store numbers in the output
+        if(el >= '0' && el <= '9')
         {
-            push(el);
+            output[j] = el;
+            j++;
         }
-        else if(el >= '0' && el <= '9')
+        // push '(' to the stack and pop only when ')' is encountered
+        else if (el == '(')
+            push(operators, el);
+        else if (el == ')')
         {
-            printf("%c ",el);
+            // Store operators between '(' and ')' in the output
+            while (operators->top > -1 && peek(operators) != '(')
+            {
+                output[j] = pop(operators);
+                j++;
+            }  
+            
+            // pop '('
+            pop(operators);
         }
-        else
+        // Remove operators from the stack which are greater than or equal to in precedence than that of the current operator
+         else
         {
-            printf("%c ",pop());
-            pop();
+            while (operators->top > -1 && priority(el) <= priority(peek(operators)))
+            {
+                output[j] = pop(operators);
+                j++;
+            }
+            // Store the current operator
+            push(operators, el);
         }
+
         i++;
     }
+
+    // pop all the remaining operators from the stack
+    while (operators->top > -1)
+    {
+        output[j] = pop(operators);
+        j++;
+    }
+
+    output[j] = '\0';
+    printf("%s", output);
+
     return ;
-    
 }
+
 int main()
 {
-    char s[] = "1+2*3-(4/5+6*7*8)";
+    char input[] = "(1+((3-1)*(4/2)))";
 
-    postfix(s);
+    postfix(input);
     return 0;
 }
-
