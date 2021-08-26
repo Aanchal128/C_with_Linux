be42f3e4dc532495a4de8c3a198ec7fc212d33e8
vagrant <vagrant@myvm18>
2021-08-26 11:41:30 +0000

    Fixed error

--- Bits_Bytes/base32_base64.c
+++ Bits_Bytes/base32_base64.c
@@ -5,22 +5,70 @@
  * 
  * Assignment 3 - Bits and Bytes
  * @Aanchal, 1910990158, 18/08/2021
+ *
  * Resources: 
  * "https://www.dcode.fr/base-32-encoding"
  * "https://www.dcode.fr/base-64-encoding"
  */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int toDecimal(char c)
 {
     int a = 0;
-    if (c >= 'A' && c <= 'Z')
+
+    if(c >= 'A' && c <= 'Z')
         a = (c - 'A');
-    else if (c >= '2' && c <= '7')
+    else if(c >= '2' && c <= '7')
         a = (26 + (c - '2'));
+
     return a;
+}
+
+int* b32_to_binary(char input[], int len, int* binlen)
+{
+    int *bin = (int*)malloc(sizeof(int) * 40000);
+
+    int i = 0, bl = 0;
+    while(i < len)
+    {
+        char c = input[i];
+        if(c != '=')
+        {
+            int dec = toDecimal(c);
+            // 5 bit binary
+            for(int j = 4; j >= 0; j--)
+            {
+                int k = ((dec >> j) & 1) ? 1 : 0;
+                bin[bl] = k;
+                bl++;
+            }
+        }
+        else
+            break;
+        i++;
+    }
+
+    // while decoding Base32 message is divided into bytes (8-bits) and remaining bits are discarded
+    // removing remaining bits
+    bl = 8*(bl/8);
+    // while encoding message into Base64-->message is split into 6 bits
+    if(bl%6 != 0)
+    {
+        int new_bl = 6*((bl/6) + 1);
+        // adding 0's
+        while(bl < new_bl)
+        {
+            bin[bl] = 0;
+            bl++;
+        }
+        bl = new_bl;
+    }
+    *binlen = bl;
+
+    return bin;
 }
 
 char encode(int value)
@@ -39,94 +87,67 @@ char encode(int value)
         return '=';
 }
 
-void conversion()
+char* binary_to_b64(int bin[], int bin_len)
 {
-    char str[200];
-    scanf("%s", str);
+    // Base 64 only works with groups of 4 characters, if needed, fill with =
+    int final_len = (bin_len/6);
+    if(final_len % 4 != 0)
+        final_len = 4*( ((bin_len/6) / 4) + 1);
 
-    int len = strlen(str);
-    while(str[len-1] == '=')
-        len -= 1;
-    
-    int c = 0;
-    // To store binary rep of base32 encoded string
-    char bin[len*5];
-    
-    // Decode
-    int i = 0;
-    while(i < len)
-    {   // To convert base32 encoded string to binary string
-        int dec = toDecimal(str[i]);
-        for (int j = 4; j >= 0; j--) 
-        {
-            int k = dec >> j;
-            if (k & 1)
-            {
-                bin[c] = '1';
-                c++;
-            }
-            else
-            {
-                bin[c] = '0';
-                c++;
-            }
-        }
-        i++;
-    }
+    char *b64 = (char*)malloc(sizeof(char) * final_len);
 
-    // To fill extra 0's for base64 encoding
-    int newlen = c; 
-    if(newlen % 6 !=0)
-        newlen += (6 - (c % 6));
-        
-    if(c %6 != 0)
-    {
-        for(int y = c; y < newlen ; y++)
-            bin[y] = '0';
-    }
-
-    // Encode
-    int l_64 = (newlen/6);
-    // Base 64 only works with groups of 4 characters, if needed, fill with = 
-    if(l_64 % 4 !=0)
-        l_64 += (4-(l_64)%4);
-
-    char b64[l_64];
-
-    int j = 0, k = 5;
-    int x = 0;
-
-    // Converting binary string to base64 encoded string
-    while(k < newlen)
+    int j = 5, k = 0, i = 0;
+    while(j < bin_len)
     {
         int value = 0, base = 1;
-        for (int i = k; i >= j; i--) 
+        // taking 6 bits at a time --> ascii
+        while(j >= i)
         {
-            if (bin[i] == '1')
+            if (bin[j])
                 value += base;
             base = base * 2;
+            j--;
         }
-        b64[x] = encode(value);
-        x++;
-        j = k+1;
-        k = j+5;
+        b64[k] = encode(value);
+        k++;
+        i += 6;
+        j = i+5;
     }
 
-    printf("%s",b64);
-
-    // fill remaining with "="
-    while(x < l_64)
+    // padding
+    while(k < final_len)
     {
-        printf("=");
-        x++;
+        b64[k] = '=';
+        k++;
     }
-    printf("\n");
+    b64[k] = '\0';
+
+    return b64;
+}
+
+void conversion()
+{
+    char input[200];
+    scanf("%s", input);
+    int len = strlen(input);
+
+    // Base32 -> binary
+    int bin_len = 0;
+    int *bin = b32_to_binary(input, len, &bin_len);
+    
+    // binary -> Base64
+    char *b_64 = binary_to_b64(bin, bin_len);
+
+    printf("%s\n",b_64);
 }
 
 int main()
 {
     int tc;
     scanf("%d",&tc);
+
     while(tc--)
         conversion();
+
+    return 0;
 }
